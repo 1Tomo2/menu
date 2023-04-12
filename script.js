56d1692fb8a1b9c7291d9e00233e4da5eb66023f
1Tomo2 <127761287+1Tomo2@users.noreply.github.com>
2023-04-12 15:06:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector(".logo i");
+let links1 = document.querySelector(".links ul");
+let links2 = document.querySelector(".links2");
+
+menu.addEventListener("click", () => {
+  links1.classList.toggle("toggle");
+});
